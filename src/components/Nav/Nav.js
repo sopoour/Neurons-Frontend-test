e5163990c8f3f214cf6f40ce281d4900dfa5b3bd
sopoour <sophia.auer@googlemail.com>
2021-07-20 17:07:06 +0200

    Final styling of cast/episode button, searchbar/filter

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,8 +7,16 @@ const Nav  = ({setIsCast, setIsEpisode}) => {
     return(
         <div className='nav'>
           <img src='logo-pied-piper.png' className="appLogo" alt="logo" />
-          <button id='cast' className='actBtn' onClick={() => {setIsCast(true); setIsEpisode(false)}}>Cast</button>
-          <button id='episodes' className='inactBtn' onClick={() =>{setIsEpisode(true); setIsCast(false)} }>Episodes</button>  
+          <button id='cast' className='actBtn' onClick={() => {
+            setIsCast(true); 
+            setIsEpisode(false)
+            document.getElementById("cast").className="actBtn";
+            document.getElementById('episodes').className="inactBtn"}}>Cast</button>
+          <button id='episodes' className='inactBtn' onClick={() =>{
+            setIsEpisode(true); 
+            setIsCast(false)
+            document.getElementById("episodes").className="actBtn";
+            document.getElementById('cast').className="inactBtn"} }>Episodes</button>  
         </div>
     );
 };
